532693237abd0e03e6728fd8b581d46c70c99441
qiyu1987 <qiyu1987@gmail.com>
2019-11-10 22:12:19 +0100

    only connect to meetup once

--- index.js
+++ index.js
@@ -8,50 +8,47 @@ var mup = new Meetup()
 
 // outside of our stream (!) we setup an empty object
 let topicsCounter = {}
+mup.stream("/2/rsvps", stream => {
+	stream
+		.on("data", item => {
+			console.log("got item " + item)
+
+			// inside of our stream event handler (!) we retrieve the group topics
+			const topicNames = item.group.group_topics.map(topic => topic.topic_name)
+			if (topicNames.includes("Software Development")) {
+				topicNames.forEach(name => {
+					if (topicsCounter[name]) {
+						topicsCounter[name]++
+					} else {
+						topicsCounter[name] = 1
+					}
+				})
+			}
+			const arrayOfTopics = Object.keys(topicsCounter)
+			let topTenTopicsCounter = {}
+
+			arrayOfTopics
+				.sort((topicA, topicB) => {
+					if (topicsCounter[topicA] > topicsCounter[topicB]) {
+						return -1
+					} else if (topicsCounter[topicB] > topicsCounter[topicA]) {
+						return 1
+					} else {
+						return 0
+					}
+				})
+				.slice(0, 9)
+				.map(topic => {
+					topTenTopicsCounter[topic] = topicsCounter[topic]
+				})
+
+			console.log(topTenTopicsCounter)
+			io.emit("action", topTenTopicsCounter)
+		})
+		.on("error", e => {
+			console.log("error! " + e)
+		})
+})
 io.on("connection", socket => {
 	console.log("got connection")
-
-	mup.stream("/2/rsvps", stream => {
-		stream
-			.on("data", item => {
-				console.log("got item " + item)
-
-				// inside of our stream event handler (!) we retrieve the group topics
-				const topicNames = item.group.group_topics.map(
-					topic => topic.topic_name
-				)
-				if (topicNames.includes("Software Development")) {
-					topicNames.forEach(name => {
-						if (topicsCounter[name]) {
-							topicsCounter[name]++
-						} else {
-							topicsCounter[name] = 1
-						}
-					})
-				}
-				const arrayOfTopics = Object.keys(topicsCounter)
-				let topTenTopicsCounter = {}
-
-				arrayOfTopics
-					.sort((topicA, topicB) => {
-						if (topicsCounter[topicA] > topicsCounter[topicB]) {
-							return -1
-						} else if (topicsCounter[topicB] > topicsCounter[topicA]) {
-							return 1
-						} else {
-							return 0
-						}
-					})
-					.slice(0, 9)
-					.map(topic => {
-						topTenTopicsCounter[topic] = topicsCounter[topic]
-					})
-
-				console.log(topTenTopicsCounter)
-				socket.emit("action", topTenTopicsCounter)
-			})
-			.on("error", e => {
-				console.log("error! " + e)
-			})
-	})
 })
